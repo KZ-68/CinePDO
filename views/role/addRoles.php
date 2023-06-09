32930e604ce34d7639ffbfab47411f67a20aeb6f
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-10 00:35:14 +0200

    feat: add file for insert new role

---
+++ views/role/addRoles.php
@@ -0,0 +1,27 @@
+<?php
+ob_start();
+// démarre la temporisation de sortie
+?>
+
+<section class="section_addRoles">
+
+<h2 id='h2_addRoles'>Ajout Rôle</h2>
+
+    <div class="addRoles_wrapper">
+        <form class='formular_base' action="" method="post">
+            <div class="nom_role">
+                <label for="nom_role">Nom du Role :</label>
+                <input type="text" name="nom_role" id="nom_role" required>
+            </div>
+            <input id="submit" type="submit" name="addRole" value="Ajouter">
+        </form>
+    </div>
+
+</section>
+
+<?php
+
+$title = "Ajout d'un Rôle";
+$content = ob_get_clean();
+require "views/template.php"
+?>
