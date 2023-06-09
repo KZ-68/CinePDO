ad13d611eb772e3770b4daad9e1eabe1cf9f9500
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-09 19:03:59 +0200

    feat: add a new view for insert a genre

---
+++ views/genre/addGenres.php
@@ -0,0 +1,27 @@
+<?php
+ob_start();
+// démarre la temporisation de sortie
+?>
+
+<section class="section_addGenres">
+
+<h2 id='h2_addFilms'>Ajout Genre</h2>
+
+    <div class="addFilms_wrapper">
+        <form class='formular_base' action="" method="post">
+            <div class="libelle">
+                <label for="libelle">Libellé du Genre :</label>
+                <input type="text" name="libelle" id="libelle" required>
+            </div>
+            <input id="submit" type="submit" name="addGenre" value="Ajouter">
+        </form>
+    </div>
+
+</section>
+
+<?php
+
+$title = "Ajout d'un genre";
+$content = ob_get_clean();
+require "views/template.php"
+?>
