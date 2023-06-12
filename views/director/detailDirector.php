98eacfc9f9bcd539c2fd28df7faa60593bab579f
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-13 00:54:05 +0200

    fix:change var names fix display films by director

--- views/director/detailDirector.php
+++ views/director/detailDirector.php
@@ -16,15 +16,15 @@ if ($director = $director->fetch()) {
 <h4>Filmographie du réalisateur</h4>
 <section class="section_director1">
 <?php  
-    while ($film = $films->fetch()) {
-        echo "<figure class='figure_film'>
-        <img class='posterMovie' src='./public/image/{$film['affiche_film']}'>"
-    ?>
-        <a class='link_film' href='index.php?action=detailFilm&id=<?=$film['id_film']?>'>
-        <h5><?=$film['titre']?></h5></a>
-    <?php
-        echo "</figcaption>
-        </figure>";
+    while ($director = $directorFilms->fetch()) {
+            echo "<figure class='figure_film'>
+            <img class='posterMovie' src='./public/image/{$director['affiche_film']}'>"
+        ?>
+            <a class='link_film' href='index.php?action=detailFilm&id=<?=$director['id_film']?>'>
+            <h5><?=$director['titre']?></h5></a>
+        <?php
+            echo "</figcaption>
+            </figure>";
     }
 }
 ?>
