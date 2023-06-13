416284e47794905990a94b8cc7bc76f4da99ffde
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-13 22:49:01 +0200

    refactor: add and delete links relocated

--- views/movie/listFilms.php
+++ views/movie/listFilms.php
@@ -9,27 +9,11 @@ ob_start();
 
 <?= "<p>{$films->rowcount()}</p>" ?>
 
-<?php
-while ($film = $films->fetch()) {
+<a href='index.php?action=deleteFilms'>
+    <h3>Supprimer Film</h3>
+    </a>
 
-    echo "<div class='card_website'>
-    <p>{$film["id_film"]}</p>";
-?>
-    
-<a href="index.php?action=detailFilm&id=<?=$film['id_film']?>">
-    <!-- <p>Detail Film</p> -->
-    <h3><?= $film["titre"] ?></h3>
-</a>
-
-<?php
-    echo "<img class='posterMovie' src='./public/image/{$film['affiche_film']}'>";
-?>
-  </div>  
-<?php
-}
-
-?>
-    <a class='addFilms_link' href='index.php?action=addFilms'>
+    <a class='addFilms_link' href='index.php?action=filmsForm'>
         <p class='addFilms_btn'>Ajouter Films
         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-plus-square' viewBox='0 0 16 16'>
             <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/>
@@ -39,6 +23,23 @@ while ($film = $films->fetch()) {
     </a>
 
 <?php
+while ($film = $films->fetch()) {
+
+    echo "<div class='card_website'>
+    <p>{$film["id_film"]}</p>";
+?>
+
+<a href="index.php?action=detailFilm&id=<?=$film['id_film']?>">
+    <!-- <p>Detail Film</p> -->
+    <h3><?= $film["titre"] ?></h3>
+</a>
+
+<?php
+    echo "<img class='posterMovie' src='./public/image/{$film['affiche_film']}'>";
+?>
+  </div>
+<?php
+}
 
 ?>
 
