58b2a753dc70e3bff029e9683a5b7f73e23a15ce
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-14 20:49:24 +0200

    fix: relocate select sql in deleteGenres method

--- views/genre/deleteGenres.php
+++ views/genre/deleteGenres.php
@@ -8,14 +8,8 @@ ob_start();
     <label for="id_genre">Supprimer un genre :</label>
     <select name="id_genre" id="id_genre" required>
     <?php
-        // Récupérer la liste des réalisateurs depuis ma base de données
-        $dao = new DAO();
-        $sql = "SELECT g.id_genre, g.libelle
-                FROM genre g";
-        $result = $dao->executerRequete($sql);
-        
         // Parcourir les résultats et afficher les options de la liste déroulante
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        while ($row = $genre->fetch(PDO::FETCH_ASSOC)) {
             $idGenre = $row['id_genre'];
             $libelle = $row['libelle'];
             echo "<option value='$idGenre'>$libelle</option>";
