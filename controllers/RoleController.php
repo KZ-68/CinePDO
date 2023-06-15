f9a07e9107f85dbe6eaa45a6e09e130258f68c07
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-15 12:54:28 +0200

    feat: add deleteRoles and editRoles method editRoles incomplete

--- controllers/RoleController.php
+++ controllers/RoleController.php
@@ -88,16 +88,21 @@ class RoleController{
         require "views/role/detailRole.php";
     }
 
+    public function openRolesForm() {
+        
+        $dao = new DAO();
+
+        require "views/role/rolesForm.php";
+    }
+
     public function addRoles(){
         
         $dao = new DAO();
 
-        // vérifie si la table de la méthode POST existe
-        if (isset($_POST['addRole'])) {
-            $nomRole = $_POST['nom_role'];
-
         $sql = "INSERT INTO role (nom_role) 
         VALUES (:nom_role)";
+
+        $nomRole = filter_input(INPUT_POST, 'nom_role', FILTER_SANITIZE_FULL_SPECIAL_CHARS);  
 
         $params = [
             ":nom_role" => $nomRole
@@ -105,9 +110,68 @@ class RoleController{
 
         $addRole = $dao->executerRequete($sql, $params);
 
-        }
+        // Comme la session est déjà démarré dans les autres fichiers, on peut créer un tableau de $_SESSION pour afficher un message
+        $_SESSION['flash_message'] = "Le rôle de " .$nomRole. " à été ajouté avec succès !";
+        // Retourne l'objet en cours et réaffiche la liste des genres 
+        $this->findAllRoles();
+    }
 
-        require "views/role/addRoles.php";
+    public function editRoles($array) {
+        $dao = new DAO();
+
+        $sql = "SELECT
+                    c.id_acteur,
+                    p.prenom,
+                    p.nom
+                FROM casting c
+                INNER JOIN acteur a ON a.id_acteur = c.id_acteur
+                INNER JOIN personne p ON p.id_personne = a.id_personne";
+        
+        $castingActor = $dao->executerRequete($sql);
+
+        $sql2 ="UPDATE role SET 
+                    id_role = :id_role, 
+                    nom_role = :nom_role";
+
+            $idRole = filter_var($array['id_role'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $nomRole = filter_var($array['nom_role'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            $params = [
+                ":id_role" => $idRole,
+                ":nom_role" => $nomRole
+            ];
+
+        $editRole = $dao->executerRequete($sql2, $params);
+    }
+
+    public function deleteRoles() {
+
+        $dao = new DAO();
+
+        $sql = "SELECT ro.id_role, ro.nom_role
+                FROM role ro";
+
+        $roles = $dao->executerRequete($sql);
+
+        // vérifie si la table de la méthode POST existe
+        if (isset($_POST['deleteRole'])) {
+            $idRole = $_POST['id_role'];
+
+            $sql2 ="DELETE FROM role ro
+                    WHERE ro.id_role = :id_role;
+
+                    DELETE FROM casting c
+                    WHERE c.id_role = :id_role";
+
+            $params = [
+                ":id_role" => $idRole 
+            ];
+
+            $delete = $dao->executerRequete($sql2, $params);
+        }
+        
+
+        require "views/role/deleteRoles.php";
     }
 }
 
