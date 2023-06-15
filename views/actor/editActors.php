f373624845a061266444c7d4772ef8ba4b40a8ee
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-15 18:44:33 +0200

    feat: add edit actors view

---
+++ views/actor/editActors.php
@@ -0,0 +1,45 @@
+<?php
+ob_start();
+// démarre la temporisation de sortie
+?>
+
+<section class="section_editActors">
+
+<h2 id='h2_editActors'>Editer Acteur</h2>
+
+    <div class="editActors_wrapper">
+        <form class='formular_base' action="" method="post">
+            
+            <label for="photo">Photo :</label>
+            <input type="file" name="photo" id="photo">
+        
+            <div class="prenom">
+                <label for="prenom">Prénom :</label>
+                <input type="text" name="prenom" id="prenom" required>
+            </div>
+
+            <div class="nom">
+                <label for="nom">Nom :</label>
+                <input type="text" name="nom" id="nom" required>
+            </div>
+            <div class="sexe">
+                <label for="duree">Sexe :</label>
+                <input type="text" name="sexe" id="sexe" required>
+            </div>
+            <div class="date_naissance">
+                <label for="date_naissance">Note :</label>
+                <input type="date" id="date_naissance" name="date_naissance" value="date_naissance" required>
+            </div>
+
+            <input id="submit" type="submit" name="editActor" value="Editer">
+        </form>
+    </div>
+
+</section>
+
+<?php
+
+$title = "Ajout d'une Personne ";
+$content = ob_get_clean();
+require "views/template.php"
+?>
