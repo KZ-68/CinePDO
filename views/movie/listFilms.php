8094058309d91ebeb918b5343ce821e1e6fb2684
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-16 12:04:57 +0200

    feat: add anchor link in loop for addCastings

--- views/movie/listFilms.php
+++ views/movie/listFilms.php
@@ -11,16 +11,16 @@ ob_start();
 
 <a href='index.php?action=deleteFilms'>
     <h3>Supprimer Film</h3>
-    </a>
+</a>
 
-    <a class='addFilms_link' href='index.php?action=filmsForm'>
-        <p class='addFilms_btn'>Ajouter Films
-        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-plus-square' viewBox='0 0 16 16'>
-            <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/>
-            <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/>
-        </svg>
-        </p>
-    </a>
+<a class='addFilms_link' href='index.php?action=filmsForm'>
+    <p class='addFilms_btn'>Ajouter Films
+    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-plus-square' viewBox='0 0 16 16'>
+        <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/>
+        <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/>
+    </svg>
+    </p>
+</a>
 
 <?php
 while ($film = $films->fetch()) {
@@ -28,6 +28,11 @@ while ($film = $films->fetch()) {
     echo "<div class='card_website'>
     <p>{$film["id_film"]}</p>";
 ?>
+
+<a href='index.php?action=addCastings&id=<?=$film['id_film']?>'>
+    <h3>Ajouter un casting</h3>
+</a>
+
 
 <a href="index.php?action=detailFilm&id=<?=$film['id_film']?>">
     <!-- <p>Detail Film</p> -->
