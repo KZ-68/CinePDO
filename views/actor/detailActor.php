ef13035509ccb8087a788f7de8efac5fef1aa677
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-16 12:02:54 +0200

    feat: add anchor link for editActors

--- views/actor/detailActor.php
+++ views/actor/detailActor.php
@@ -4,10 +4,15 @@ ob_start();
 ?>
 
 <?php
-if ($actor = $actor->fetch()) {
 
+if ($actor = $actor->fetch()) {
+    
     // détail de l'acteur
     echo "<div class='card_website'>
+    <a href='index.php?action=editActors&id={$actor['id_personne']}'>
+    <h3>Modifier cet acteur</h3>
+    </a></p>
+    
     <img class='photoPerson' src='public/image/{$actor["photo"]}'<br/>
         Prénom et Nom de l'acteur : {$actor["prenom"]} {$actor["nom"]}<br/>
         Sexe : {$actor["sexe"]}<br/>
@@ -21,7 +26,8 @@ if ($actor = $actor->fetch()) {
 <?php  
     while ($film = $filmsActor->fetch()) {
         echo "<figure class='figure_film'>
-        <img class='posterMovie' src='./public/image/{$film['affiche_film']}'>"
+        <img class='posterMovie' src='./public/image/{$film['affiche_film']}'>
+        "
     ?>
         <a class='link_film' href='index.php?action=detailFilm&id=<?=$film['id_film']?>'>
         <h5><?=$film['titre']?></h5></a>
