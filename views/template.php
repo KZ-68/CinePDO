8eb4a71a01871606e5c736a52404ebaeb0482c8f
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-19 18:24:07 +0200

    style: change footer and nav tags structure

--- views/template.php
+++ views/template.php
@@ -11,38 +11,36 @@
 <body>
     <header>
         <h1 id="kzcine">KZCine</h1>
-        <nav>
+        <nav class="nav_top">
             <ul>
-                <li><a class="navTop" href="index.php?action=homepage">Home</a></li>
-                <li><a class="navTop" href="index.php?action=listFilms">Movies List</a></li>
-                <li><a class="navTop" href="index.php?action=listActors">Actors List</a></li>
-                <li><a class="navTop" href="index.php?action=listDirectors">Directors List</a></li>
-                <li><a class="navTop" href="index.php?action=listGenres">Genres List</a></li>
-                <li><a class="navTop" href="index.php?action=listRoles">Roles List</a></li>
-                <li><a class="navTop" href="index.php?action=listPersons">Persons List</a></li>
+                <li><a class="liTop" href="index.php?action=homepage">Home</a></li>
+                <li><a class="liTop" href="index.php?action=listFilms">Movies List</a></li>
+                <li><a class="liTop" href="index.php?action=listActors">Actors List</a></li>
+                <li><a class="liTop" href="index.php?action=listDirectors">Directors List</a></li>
+                <li><a class="liTop" href="index.php?action=listGenres">Genres List</a></li>
+                <li><a class="liTop" href="index.php?action=listRoles">Roles List</a></li>
+                <li><a class="liTop" href="index.php?action=listPersons">Persons List</a></li>
             </ul>
         </nav>
     </header>
 
     <main>
-    <div class="flash">
-    <?php
-        if(isset($_SESSION['flash_message'])) {
-            $message = $_SESSION['flash_message'];
-            echo $message;
-            unset($_SESSION['flash_message']);
-        }
-    ?>
-    </div>
-    <?= $content ?>
+        <div class="content_wrapper">
+            <div class="flash">
+            <?php
+                if(isset($_SESSION['flash_message'])) {
+                    $message = $_SESSION['flash_message'];
+                    echo $message;
+                    unset($_SESSION['flash_message']);
+                }
+            ?>
+            </div>
+            <?= $content ?>
+        </div>
     </main>
 
     <footer>
         <div class="footer_container">
-            <hgroup id="footer_group">
-                <h3 id="footer_title">KZCine</h3>
-                <small>Copyright Kevin ZITNIK</small>
-            </hgroup>
             <nav>
                 <ul>
                     <li><a class="footer_nav" href="#">ABOUT</a></li>
@@ -50,6 +48,8 @@
                     <li><a class="footer_nav" href="#">CONTACT</a></li>
                 </ul>
             </nav>
+            <h3 id="footer_title">KZCine</h3>
+            <small>Copyright Kevin ZITNIK</small>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
